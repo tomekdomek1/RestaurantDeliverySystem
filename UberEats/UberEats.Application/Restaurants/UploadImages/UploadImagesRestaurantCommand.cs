92be013a95dcf356479b8f3d653fd59b34f50829
Łukasz Mieczkowski <89240@student.pb.edu.pl>
2025-12-01 22:52:32 +0100

    Add image upload feature for restaurants

---
+++ UberEats/UberEats.Application/Restaurants/UploadImages/UploadImagesRestaurantCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using UberEats.Application.Common;
+
+namespace UberEats.Application.Restaurants.CreateRestaurant;
+
+
+public class UploadImagesRestaurantCommand : IRequest<List<UploadResult>>
+{
+    public Guid Id { get; }
+    public List<UploadFile> Images { get; } = new List<UploadFile>();
+    public UploadImagesRestaurantCommand(Guid restaurantId, List<UploadFile> images)
+    {
+        Id = restaurantId;
+        Images = images;
+    }
+}
---
+++ UberEats/UberEats.Application/Restaurants/UploadImages/UploadImagesRestaurantCommandHandler.cs
@@ -0,0 +1,58 @@
+﻿using MediatR;
+using UberEats.Application.Common;
+using UberEats.Domain.Interfaces;
+using UberEats.Domain.Repository;
+
+namespace UberEats.Application.Restaurants.CreateRestaurant;
+
+public class UploadImagesRestaurantCommandHandler : IRequestHandler<UploadImagesRestaurantCommand, List<UploadResult>>
+{
+    private readonly IRestaurantRepository _restaurantRepository;
+    private readonly IFileStorage _fileStorage;
+    public UploadImagesRestaurantCommandHandler(IRestaurantRepository restaurantRepository, IFileStorage fileStorage)
+    {
+        _restaurantRepository = restaurantRepository;
+        _fileStorage = fileStorage;
+    }
+    public async Task<List<UploadResult>> Handle(UploadImagesRestaurantCommand request, CancellationToken cancellationToken)
+    {
+        var restaurant = await _restaurantRepository.GetByIdAsync(request.Id);
+        if (restaurant == null)
+        {
+            throw new Exception("Restaurant not found"); // TODO: Custom exception
+        }
+        var result = new List<UploadResult>();
+
+        foreach (var file in request.Images)
+        {
+            if (!_fileStorage.IsFileValid(file.ContentType, file.Length))
+            {
+                result.Add(new UploadResult
+                {
+                    OriginalFileName = file.FileName,
+                    IsSuccess = false,
+                    ErrorMessage = "Invalid file. Only jpeg is allowed. Max size is 5MB"
+                });
+                continue;
+            }
+
+            // Dont know if filepath should be stored in DB or not, propably it should
+            var imageId = Guid.NewGuid();
+            var path = $"{restaurant.Id}/CommonImages/{imageId}.{file.ContentType.Split('/')[1]}";
+
+            await _fileStorage.SaveAsync(file.Content, path);
+
+            result.Add(new UploadResult
+            {
+                IsSuccess = true,
+                OriginalFileName = file.FileName,
+                RelativeFilePath = path
+            });
+        }
+
+        return result;
+
+    }
+
+}
+
