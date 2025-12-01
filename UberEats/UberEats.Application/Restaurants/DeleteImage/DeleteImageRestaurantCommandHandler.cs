ed021574c73e316bd30ffacf429815f14c93c7c3
Łukasz Mieczkowski <89240@student.pb.edu.pl>
2025-12-01 22:53:27 +0100

    Add DeleteImageRestaurantCommand and its handler

---
+++ UberEats/UberEats.Application/Restaurants/DeleteImage/DeleteImageRestaurantCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using UberEats.Domain.Interfaces;
+using UberEats.Domain.Repository;
+
+namespace UberEats.Application.Restaurants.DeleteImage;
+
+public class DeleteImageRestaurantCommandHandler : IRequestHandler<DeleteImageRestaurantCommand, bool>
+{
+    private readonly IRestaurantRepository _restaurantRepository;
+    private readonly IFileStorage _fileStorage;
+
+    public DeleteImageRestaurantCommandHandler(IRestaurantRepository restaurantRepository, IFileStorage fileStorage)
+    {
+        _restaurantRepository = restaurantRepository;
+        _fileStorage = fileStorage;
+    }
+
+    public async Task<bool> Handle(DeleteImageRestaurantCommand request, CancellationToken cancellationToken)
+    {
+        var restaurant = await _restaurantRepository.GetByIdAsync(request.RestaurantId);
+        if (restaurant == null)
+        {
+            throw new Exception("Restaurant not found");  // TODO: Custom exception
+        }
+        var path = $"{restaurant.Id}/CommonImages/{request.ImageId}.jpeg";
+
+        return await _fileStorage.DeleteFileAsync(path);
+    }
+}
