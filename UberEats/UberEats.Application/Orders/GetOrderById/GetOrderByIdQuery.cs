55fadb118bbf4a64bd2bb506bd7f47e0eed0cdab
mieczkowski107 <mieczkowski107@gmail.com>
2026-05-18 22:05:20 +0200

    Refactor GetOrderById functionality by consolidating query and handler into a single file and removing unnecessary class

--- UberEats/UberEats.Application/Orders/GetOrderById/GetOrderByIdQuery.cs
+++ UberEats/UberEats.Application/Orders/GetOrderById/GetOrderByIdQuery.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MediatR;
+using UberEats.Domain.Entities;
+using UberEats.Domain.Repository;
 
-namespace UberEats.Application.Orders.GetOrderById
+namespace UberEats.Application.Orders.GetOrderById;
+
+public sealed record GetOrderByIdQuery(Guid Id) : IRequest<Order>;
+
+public sealed class GetOrderByIdQueryHandler : IRequestHandler<GetOrderByIdQuery, Order>
 {
-    internal class GetOrderByIdQuery
+    private readonly IOrderRepository _orderRepository;
+
+    public GetOrderByIdQueryHandler(IOrderRepository orderRepository)
     {
+        _orderRepository = orderRepository;
+    }
+
+    public async Task<Order> Handle(GetOrderByIdQuery request, CancellationToken cancellationToken)
+    {
+        var order = await _orderRepository.GetOrderWithDetailsAsync(request.Id);
+
+        if (order == null)
+        {
+            throw new KeyNotFoundException($"Order with ID {request.Id} not found.");
+        }
+
+        return order;
     }
 }
