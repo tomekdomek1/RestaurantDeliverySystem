186d72347d486f60671c5057144622959140fea3
mieczkowski107 <mieczkowski107@gmail.com>
2026-05-18 22:05:10 +0200

    Implement Order repository and add Order details retrieval functionality

--- UberEats/UberEats.Domain/Entities/Order.cs
+++ UberEats/UberEats.Domain/Entities/Order.cs
@@ -21,6 +21,10 @@ public sealed class Order : Entity<Guid>
     public Guid RestaurantId { get; set; }
     public Guid DriverId { get; set; }
 
+    public OrderAddress? Address { get; set; }
+    public Driver? Driver { get; set; }
+    public ICollection<OrderItem> Items { get; set; } = new List<OrderItem>();
+
     public Order(Guid id, string? notes, DateTime date, TimeOnly deliveryTime, OrderStatus orderStatus, 
                  Guid customerId, Guid restaurantId, Guid driverId) : base(id)
     {
